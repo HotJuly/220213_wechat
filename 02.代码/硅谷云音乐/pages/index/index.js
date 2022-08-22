13abcad075aa6a5836b240f5e35e4d53383fedc2
HotJuly <949588380@qq.com>
2022-08-22 10:55:01 +0800

    22.小程序数据请求

--- 02.代码/硅谷云音乐/pages/index/index.js
+++ 02.代码/硅谷云音乐/pages/index/index.js
@@ -6,13 +6,56 @@ Page({
    */
   data: {
 
+    // 用于存储首页轮播图相关数据
+    banners:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    /*
+      发送请求所需要知道的三个事情
+        1.在哪发
+          在Vue中,我们会考虑在created或者mounted中发送请求
+          小程序的onLoad类似于created
+          小程序的onReady类似于mounted
 
+          我选择:onLoad
+
+        2.怎么发
+          在Vue中,使用axios发送请求(axios的源码其实是用promise和ajax组合实现)
+            小程序中没有window对象,所以无法发送ajax请求
+            小程序的全局对象是wx
+
+          API:wx.request(Object object)
+
+            小程序中,
+              如果请求方式是GET,那么data对象就会变成query字符串进行url拼接
+              如果请求方式是POST,那么data对象就会作为请求体参数进行发送
+
+        3.往哪发
+          查看接口文档
+            请求三要素:地址+请求方式+请求参数
+    */
+
+    // console.log('window',window)
+    wx.request({
+      url:"http://localhost:3000/banner",
+      method:"GET",
+      data:{
+        type:2
+      },
+      success:(res)=>{
+        // 此处的res是响应报文对象,其中的data才是响应体数据
+        // console.log('res',res)
+        const banners = res.data.banners;
+        this.setData({
+          // banners:banners,
+          banners
+        })
+      }
+    })
   },
 
   /**
