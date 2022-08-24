4392aa1fb275b030963ceedc47e77f4ad5671ede
HotJuly <949588380@qq.com>
2022-08-24 11:55:02 +0800

    41.视频页面导航列表交互实现

--- 02.代码/硅谷云音乐/pages/video/video.js
+++ 02.代码/硅谷云音乐/pages/video/video.js
@@ -1,11 +1,29 @@
 // pages/video/video.js
+import myAxios from '../../utils/myAxios';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    // 用于存储导航区域列表数据
+    navList:[],
 
+    // 用于记录用户点击选中的导航选项
+    currentId:null
+  },
+
+  // 用于监视用户点击导航区域选项操作
+  changeCurrentId(event){
+    // console.log('changeCurrentId',event)
+    // 获取到当前被点击标签的自定义属性data-id,从中可以知道用户选择的是哪个选项
+    const id = event.target.dataset.id;
+
+
+    // 最终将用户选中的选项id更新到data中,最终实现页面上类名和下划线的切换效果
+    this.setData({
+      currentId:id
+    })
   },
 
   /**
@@ -25,8 +43,19 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow:async function () {
+    // tabBar页面除非使用某些特殊手段,否则正常情况下会一直存在
+    // 最多是隐藏页面,不会销毁,所以选择使用onShow生命周期最稳妥
 
+    const { data } = await myAxios("/video/group/list");
+
+    // 切割前十三条数据进行展示
+    // 小程序更新数据是同步更新,但是以下代码还在解析实参的过程中,还没有触发setData内部的代码,也就是说还没有更新navList.所以不能写成以下写法
+    this.setData({
+      navList:data.slice(0,13),
+      // currentId:this.data.navList[0].id 错误写法
+      currentId:data[0].id
+    })
   },
 
   /**
